b4ba929f28c4132108b563c843db08cbcad82628
shun91 <shun9167@gmail.com>
2023-09-29 22:09:06 +0900

    add @google-cloud/functions-framework

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as functions from "@google-cloud/functions-framework";
+
+type RequestBody = {
+  text: string;
+  userName: string;
+  linkToTweet: string;
+  createdAt: string;
+  type: "tweet" | "like";
+};
+
+const accessToken = process.env.ACCESS_TOKEN;
+
+export const cleanSpotArea = functions.http(
+  "cleanSpotArea",
+  async (req: functions.Request, res: functions.Response) => {
+    // accessTokenが不正なら401を返す
+    // TODO: あとでコメントアウト外す
+    // if (req.headers.authorization !== `Bearer ${accessToken}`) {
+    //   const error = { status: 401, message: "Unauthorized" };
+    //   console.error(error);
+    //   res.status(401).json(error);
+    //   return;
+    // }
+
+    try {
+      console.info("cleanSpotArea ok!");
+      res.json({ status: "cleanSpotArea ok!" });
+    } catch (error: any) {
+      console.error("Unknown error:", error);
+      res.status(500).json(error);
+    }
+  }
+);
